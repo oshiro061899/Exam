c07c7a02312493b0a71203ae11ab8f8c44598520
ooi2572045 <ooi2572045@stu.o-hara.ac.jp>
2026-04-28 10:41:12 +0900

    kk

--- src/main/java/dao/SubjectDao.java
+++ src/main/java/dao/SubjectDao.java
@@ -2,34 +2,68 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
-import javax.security.auth.Subject;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import bean.School;
+import bean.Subject;
 
 public class SubjectDao extends Dao {
-
+    /**
+     * getメソッド 科目コードと学校を指定して科目インスタンスを1件取得する
+     *
+     * @param cd:String 科目コード
+     * @param school:School 学校
+     * @return 科目クラスのインスタンス 存在しない場合はnull
+     * @throws Exception
+     */
     public Subject get(String cd, School school) throws Exception {
-
+        // 科目インスタンスを初期化
         Subject subject = new Subject();
-
+        // データベースへのコネクションを確立
         Connection connection = getConnection();
+        // プリペアードステートメント
         PreparedStatement statement = null;
 
         try {
-            statement = connection.prepareStatement(
-                "select * from subject where cd = ? and school_cd = ?"
-            );
-
+            // プリペアードステートメントにSQL文をセット
+            // 科目コード(CD)と学校コード(SCHOOL_CD)の両方で絞り込む
+            statement = connection.prepareStatement("select * from subject where cd = ? and school_cd = ?");
+            // プリペアードステートメントに値をバインド
             statement.setString(1, cd);
             statement.setString(2, school.getSchoolCd());
+            // プリペアードステートメントを実行
+            ResultSet rSet = statement.executeQuery();
 
-
+            if (rSet.next()) {
+                // リザルトセットが存在する場合
+                subject.setCd(rSet.getString("cd"));
+                subject.setName(rSet.getString("name"));
+                subject.setSchool(school);
+            } else {
+                // 存在しない場合
+                subject = null;
+            }
+        } catch (Exception e) {
+            throw e;
         } finally {
-            if (statement != null) statement.close();
-            if (connection != null) connection.close();
+            // プリペアードステートメントを閉じる
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
         }
-
         return subject;
     }
 }
